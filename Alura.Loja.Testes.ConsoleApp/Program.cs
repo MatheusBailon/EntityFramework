19252c8bb2b33355c7b99807d0a178c2c6d88ab4
Matheus Henrique <matheus_henrique06@hotmail.com>
2019-11-04 20:05:36 -0300

    Processo e Log para observar a insersão de compra no banco.
    Nota: Não precisei fazer um SaveChages no produto relacionado a compra o Entity já identificou essa entrada na propriedade relacionada e a inseriu

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
@@ -22,9 +23,36 @@ namespace Alura.Loja.Testes.ConsoleApp
             };
 
             var compra = new Compra();
-            compra.Quantidade = 6;
+            compra.Quantidade = 16;
             compra.Produto = pao;
             compra.Preco = pao.PrecoUnitario * compra.Quantidade;
+
+            //Adicionando a classe de Logger
+
+            using (var contexto = new LojaContext())
+            {
+                //*** Adicionando Log (para exibir as consultas realizadas pelo Entity) ***
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+                //*** Fim do Log ***
+
+                contexto.Compras.Add(compra);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+            }
+
+            static void ExibeEntries(IEnumerable<EntityEntry> entries)
+            {
+                foreach (var item in entries)
+                {
+                    Console.WriteLine($"{item.Entity.ToString()} - {item.State}");
+                }
+            }
+
+            Console.ReadLine();
         }
 
     }
